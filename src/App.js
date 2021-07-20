6d83badbf175175a3e8d4987ffef47dedc2e99cd
Ivan Burdolenko <scientistfromlambdacomplex@gmail.com>
2021-07-20 18:01:11 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import Card from './Card';
 import Header from './Header';
-import Elem from './Elem';
 
 function App() {
     return (
         <div>
             <Header />
             <Card />
-            <Elem />
         </div>
     );
 }
