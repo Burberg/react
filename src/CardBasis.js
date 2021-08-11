43cfc263c2b7215de94a2e08feb039d01b37c869
Burberg <scientistfromlambdacomplex@gmail.com>
2021-08-11 10:08:28 +0300

    task-7

--- src/CardBasis.js
+++ src/CardBasis.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Checkbox from './common/components/Checkbox';
+import Checkbox from './Common/Components/Checkbox';
 import CardList from './cardcomponents/CardList';
 
 class CardBasis extends React.Component {
